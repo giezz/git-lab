069c07a8f3c73c66a5b11cd6e07e53dfa18a09b3
giezz <giezz@vk.com>
2023-10-13 00:27:55 +0300

    Some changes

--- src/main/java/ru/vyatsu/Main.java
+++ src/main/java/ru/vyatsu/Main.java
@@ -2,6 +2,7 @@ package ru.vyatsu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        System.out.println("Hello again");
     }
 }
