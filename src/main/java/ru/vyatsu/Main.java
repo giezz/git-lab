9d2e9422dadf2ecf89c1a14926ac31926ce0a95d
giezz <giezz@vk.com>
2023-10-13 00:02:41 +0300

    commit

--- src/main/java/ru/vyatsu/Main.java
+++ src/main/java/ru/vyatsu/Main.java
@@ -3,10 +3,5 @@ package ru.vyatsu;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
-        }
     }
 }
